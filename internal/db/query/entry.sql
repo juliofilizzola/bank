e08ccd94e7e1dd0f261153631f85f3b1ca428be5
julio_filizzola <juliofilizzola@hotmail.com>
2023-06-28 09:02:26 -0300

    feat: create create entries

---
+++ internal/db/query/entry.sql
@@ -0,0 +1,8 @@
+-- name: CreateEntry :exec
+INSERT INTO entries (account_id, amount) VALUE (?, ?);
+
+-- name: GetEntry :one
+SELECT * FROM entries WHERE id = ?;
+
+-- name: ListEntries :many
+SELECT * FROM entries WHERE account_id = ?;
