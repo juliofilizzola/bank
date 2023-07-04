ee7668d253b13aa88644970158999dbe0aded07e
julio_filizzola <juliofilizzola@hotmail.com>
2023-07-04 08:31:08 -0300

    feat: create router get account

--- api/server.go
+++ api/server.go
@@ -14,6 +14,7 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 	router.POST("account", server.CreateAccount)
+	router.GET("account/:id", server.GetAccount)
 	server.router = router
 	return server
 }
